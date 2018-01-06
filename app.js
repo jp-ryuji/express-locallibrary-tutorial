9053729c05a16c3f7df09f70692c8c3a7da09f4d
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-01-06 20:54:06 +0900

    Remove useMongoClient WARNING: The `useMongoClient` option is no longer necessary in mongoose 5.x, please remove it.

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ var app = express()
 // Set up mongoose connection
 var mongoose = require('mongoose')
 var mongoDB = 'mongodb://localhost/locallibrary'
-mongoose.connect(mongoDB, {
-  useMongoClient: true
-})
+mongoose.connect(mongoDB)
 mongoose.Promise = global.Promise
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
